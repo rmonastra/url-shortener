5eaab8f1d75f9bdbafa4a1a8bd17fb4f13ceb5a9
Richard Monastra <rmonastra2@gmail.com>
2017-06-29 15:05:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ let app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({
-  extended: false
+  extended: true
 }));
-app.use('/public',express.static(process.cwd() + '/public'));
+app.use('/public', express.static(process.cwd() + '/public'));
+
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/public/index.html');
@@ -27,41 +28,43 @@ mongoose.Promise = global.Promise;
 let dbUrl = process.env.MONGOLAB_URI;
 mongoose.connect(dbUrl || 'mongodb://localhost/shortUrls');
 
-//POST method to store and get a short url
-app.post('/api/shorturl/new', (req, res) => {
-  // Regexp for domains
-  let urlRegex = /(?:[\w-]+\.)+[\w-]+/;
-  let findUrl = urlRegex.exec(req.body.url)[0];
-  dns.lookup(findUrl, (err, address, family) => {
-    console.log(findUrl);
-    if (address) {
-       let shortId = randomstring.generate(6);
-      let postedUrl = new shortUrl({
-        original_url: findUrl,
+app.post('/api/shorturl/new/', (req, res) => {
+  
+// Regexp for domains
+  let userUrl = req.body.url;
+  let formatRegex =/(?:[\w-]+\.)+[\w-]+/;
+  let findUrl = formatRegex.test(userUrl);
+
+  dns.lookup(userUrl, () => {
+
+    let shortId = randomstring.generate(6);//generates 6 digit id for short url.
+
+    if (findUrl === true) {
+      let postedUrl = new shortUrl({//user url
+        original_url: userUrl,
         short_url: shortId
       });
-      postedUrl.save((err, url) => {
+      postedUrl.save((err, url) => { //saves to database
         res.json({
-          original_url: findUrl,
+          original_url: userUrl,
           short_url: shortId
         });
-      })
-    } 
-    else{
+      });
+    } else{
       res.json({
-        error: "Are you sure you typed something correctly?"
+        error: "Invalid Url"
       });
     }
-  })
+  });
 });
 
-//GET method to get redirected url from short url 
 app.get("/api/shorturl/:urlShort", (req, res) => {
-  let shorter_url = req.params.urlShort;//grabs short url
+  
+  shorter_url = req.params.urlShort;//grabs short url
 
  shortUrl.findOne({ 'short_url': shorter_url })
 .then( (data) => {
-
+   
  let format = new RegExp("^(http|https)://", "i");
     let checkForShort = data.original_url;
 //if statement to check for original url from short url entered
@@ -79,6 +82,3 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Listening on port: " + port);
 });
-
-
-
